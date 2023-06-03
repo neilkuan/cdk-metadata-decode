20de7b3e97f03bc03ce63fb7572b57d096be1abc
Neil Kuan <guan840912@gmail.com>
2023-06-03 20:15:11 +0800

    chore: update approval-secret

--- .projenrc.js
+++ .projenrc.js
@@ -1,4 +1,4 @@
-const { typescript } = require('projen');
+const { typescript, github } = require('projen');
 const project = new typescript.TypeScriptProject({
   defaultReleaseBranch: 'main',
   majorVersion: 1,
@@ -8,7 +8,14 @@ const project = new typescript.TypeScriptProject({
   workflowNodeVersion: '16.14.0',
   autoApproveOptions: {
     allowedUsernames: ['auto-machine'],
-    secret: 'AUTO_MACHINE_TOKEN',
+    secret: 'PROJEN_GITHUB_TOKEN',
+  },
+  depsUpgradeOptions: {
+    workflowOptions: {
+      projenCredentials: github.GithubCredentials.fromPersonalAccessToken({
+        secret: 'AUTO_MACHINE_TOKEN',
+      }),
+    },
   },
   sampleCode: false,
   autoApproveUpgrades: true,
